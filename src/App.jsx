f7dbb7073e34061013bf7a627c0893793bd48b0c
Kochpi <raphael.picoche1@gmail.com>
2026-03-12 19:30:12 +0100

    je sais pas ce que je fais

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-
-
-
-
-
-
+import Home from "./pages/Home";
+import Service from "./pages/Service";
+import Projet from "./pages/Projet";
+import Contact from "./pages/Contact";
+import Legale from "./pages/Legale";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 
 function App() {
   return (
-    <h1>A propos</h1>
+    <Router>
+      <Header />
+      <main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/projet" element={<Projet />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/legale" element={<Legale />} />
+      </Routes>
+      </main>
+      <Footer />
+      
+    </Router>
   );
 }
 
