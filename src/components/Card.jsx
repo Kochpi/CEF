b85efd5294d0d4c9a813cc3d723eb8ed4de65554
Kochpi <raphael.picoche1@gmail.com>
2026-03-13 16:19:21 +0100

    finalisation de la première phase de correction au w3c, ajout de li span, modification css du header et footer

---
+++ src/components/Card.jsx
@@ -0,0 +1,31 @@
+import "../style/Card.css"
+
+
+function Card({ image, title, description, tech }) {
+    return (
+        <div className="col-md-4 mb-4 mt-5">
+            <div className="card-custom h-100 border-0 rounded-3">
+
+                <img
+                src={image}
+                className="card-img-top"
+                alt={title}
+                />
+
+                <div className="card-body text-center mt-4">
+                    <h3 className="card-title mt-4">{title}</h3>
+                    <p className="card-text mt-4">{description}</p>
+
+                    <button className="btn-custom">
+                        Voir le site
+                    </button>
+                </div>
+
+                <div className="card-footer mt-4 text-muted text-center">
+                    {tech}
+                </div>
+            </div>
+        </div>
+    )
+}
+export default Card;
