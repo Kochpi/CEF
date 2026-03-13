df7bcf13667273f8e711966a52c2ff6c3e2daaa3
Kochpi <raphael.picoche1@gmail.com>
2026-03-13 15:26:58 +0100

    correction du footer  pour le validateur w3c

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,22 +10,30 @@ function Footer() {
           
           <div className="col-md-4">
             <ul>
-            <h6>John Doe</h6>
+            <span>John Doe</span>
             <li><p className="mb-0">40 rue Bourzeix</p></li>
             <li><p className="mb-0">Lyon, France</p></li>
             <li><p className="mb-0">10 20 30 40 50</p></li>
             <li><p className="mb-0">john.doe@gmail.com</p></li>
-            <div className=" d-flex gap-3 mt-2 fs-4 reseaux">
-            <li><a href="https://github.com/github-john-doe" target="_blank" rel="noopener noreferrer"><i className="bi bi-github"></i></a></li>
-            <li><a href="https://www.linkedin.com/in/john-doe-90b105260/" target="_blank" rel="noopener noreferrer"><i className="bi bi-linkedin"></i></a></li>
-            <li><a href="https://x.com/elonmusk" target="_blank" rel="noopener noreferrer"><i className="bi bi-twitter-x"></i></a></li>
-            </div>
+            <li className="reseaux">
+                <a href="https://github.com/github-john-doe" target="_blank" rel="noopener noreferrer">
+                  <i className="bi bi-github"></i>
+                </a>
+
+                <a href="https://x.com/elonmusk" target="_blank" rel="noopener noreferrer">
+                  <i className="bi bi-twitter-x"></i>
+                </a>
+
+                <a href="https://www.linkedin.com/in/john-doe-90b105260/" target="_blank" rel="noopener noreferrer">
+                  <i className="bi bi-linkedin"></i>
+               </a>
+            </li>
             </ul>
           </div>
 
           <div className="col-md-4">
             <ul>
-            <h6>Liens</h6>
+            <span>Liens</span>
             <li><Link className="nav-link" to="/">Accueil</Link></li>
             <li><Link className="nav-link" to="/service">Services</Link></li>
             <li><Link className="nav-link" to="/projet">Portfolio</Link></li>
@@ -36,7 +44,7 @@ function Footer() {
 
           <div className="col-md-4">
             <ul>
-            <h6>Mes dernières réalisations</h6>
+            <span>Mes dernières réalisations</span>
             <li><Link className="nav-link" to="/projet">Fresh Food</Link></li>
             <li><Link className="nav-link" to="/projet">Restaurant Akira</Link></li>
             <li><Link className="nav-link" to="/projet">Espace bien-être</Link></li>
